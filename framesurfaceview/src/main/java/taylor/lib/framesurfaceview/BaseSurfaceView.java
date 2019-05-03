61d014c38afabc2bff00f410e7eaacad8751ee30
taylor <liang.tang@cootek.cn>
2019-05-03 22:50:12 +0800

    [feat] play frame animation by SurfaceView

---
+++ framesurfaceview/src/main/java/taylor/lib/framesurfaceview/BaseSurfaceView.java
@@ -0,0 +1,156 @@
+package taylor.lib.framesurfaceview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public abstract class BaseSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    public static final int DEFAULT_FRAME_DURATION_MILLISECOND = 50;
+
+    private HandlerThread handlerThread;
+    private SurfaceViewHandler handler;
+    private int frameDuration = DEFAULT_FRAME_DURATION_MILLISECOND;
+    private Canvas canvas;
+    private boolean isAlive;
+
+    public BaseSurfaceView(Context context) {
+        super(context);
+        init();
+    }
+
+    public BaseSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public BaseSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setFrameDuration(int frameDuration) {
+        this.frameDuration = frameDuration;
+    }
+
+    protected void init() {
+        getHolder().addCallback(this);
+        setBackgroundTransparent();
+    }
+
+    private void setBackgroundTransparent() {
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        setZOrderOnTop(true);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        isAlive = true;
+        startDrawThread();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        stopDrawThread();
+        isAlive = false;
+    }
+
+    private void stopDrawThread() {
+        handlerThread.quit();
+        handler = null;
+    }
+
+    private void startDrawThread() {
+        handlerThread = new HandlerThread("SurfaceViewThread");
+        handlerThread.start();
+        handler = new SurfaceViewHandler(handlerThread.getLooper());
+        handler.post(new DrawRunnable());
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int originWidth = getMeasuredWidth();
+        int originHeight = getMeasuredHeight();
+        int width = widthMode == MeasureSpec.AT_MOST ? getDefaultWidth() : originWidth;
+        int height = heightMode == MeasureSpec.AT_MOST ? getDefaultHeight() : originHeight;
+        setMeasuredDimension(width, height);
+        Log.v("ttaylor", "BaseSurfaceView.onMeasure()" + "  default Width=" + getDefaultWidth() + " default height=" + getDefaultHeight());
+    }
+
+    /**
+     * the width is used when wrap_content is set to layout_width
+     * the child knows how big it should be
+     *
+     * @return
+     */
+    protected abstract int getDefaultWidth();
+
+    /**
+     * the height is used when wrap_content is set to layout_height
+     * the child knows how big it should be
+     *
+     * @return
+     */
+    protected abstract int getDefaultHeight();
+
+
+    private class SurfaceViewHandler extends Handler {
+
+        public SurfaceViewHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    }
+
+    private class DrawRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            if (!isAlive) {
+                return;
+            }
+            try {
+                canvas = getHolder().lockCanvas();
+                onFrameDraw(canvas);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                getHolder().unlockCanvasAndPost(canvas);
+                onFrameDrawFinish();
+            }
+
+            handler.postDelayed(this, frameDuration);
+        }
+    }
+
+    /**
+     * it is will be invoked after one frame is drawn
+     */
+    protected abstract void onFrameDrawFinish();
+
+    /**
+     * draw one frame to the surface by canvas
+     *
+     * @param canvas
+     */
+    protected abstract void onFrameDraw(Canvas canvas);
+}
---
+++ framesurfaceview/src/main/java/taylor/lib/framesurfaceview/FrameSurfaceView.java
@@ -0,0 +1,194 @@
+package taylor.lib.framesurfaceview;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * a SurfaceView which draws bitmaps one after another like frame animation
+ */
+public class FrameSurfaceView extends BaseSurfaceView {
+    public static final int INVALID_BITMAP_INDEX = Integer.MAX_VALUE;
+
+    private List<Integer> bitmaps = new ArrayList<>();
+    private Bitmap frameBitmap;
+    private int bitmapIndex = INVALID_BITMAP_INDEX;
+    private Paint paint = new Paint();
+    private BitmapFactory.Options options;
+    private Rect srcRect;
+    private Rect dstRect = new Rect();
+    private int defaultWidth;
+    private int defaultHeight;
+
+    public void setDuration(int duration) {
+        int frameDuration = duration / bitmaps.size();
+        setFrameDuration(frameDuration);
+    }
+
+    public void setBitmaps(List<Integer> bitmaps) {
+        if (bitmaps == null || bitmaps.size() == 0) {
+            return;
+        }
+        this.bitmaps = bitmaps;
+        //by default, take the first bitmap's dimension into consideration
+        getBitmapDimension(bitmaps.get(0));
+    }
+
+    private void getBitmapDimension(Integer integer) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(this.getResources(), integer, options);
+        defaultWidth = options.outWidth;
+        defaultHeight = options.outHeight;
+        srcRect = new Rect(0, 0, defaultWidth, defaultHeight);
+        Log.v("ttaylor", "FrameSurfaceView.getBitmapDimension()" + "  defaultWidth=" + defaultWidth + " defaultHeight=" + defaultHeight);
+        //we have to re-measure to make defaultWidth in use in onMeasure()
+        requestLayout();
+
+    }
+
+    public FrameSurfaceView(Context context) {
+        super(context);
+    }
+
+    public FrameSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FrameSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        options = new BitmapFactory.Options();
+        options.inMutable = true;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        dstRect.set(0, 0, getWidth(), getHeight());
+    }
+
+    @Override
+    protected int getDefaultWidth() {
+        return defaultWidth;
+    }
+
+    @Override
+    protected int getDefaultHeight() {
+        return defaultHeight;
+    }
+
+    @Override
+    protected void onFrameDrawFinish() {
+//        recycle();
+    }
+
+    /**
+     * recycle the bitmap used by frame animation.
+     * Usually it should be invoked when the ui of frame animation is no longer visible
+     */
+    public void recycle() {
+        if (frameBitmap != null) {
+            frameBitmap.recycle();
+            frameBitmap = null;
+        }
+    }
+
+    @Override
+    protected void onFrameDraw(Canvas canvas) {
+        clearCanvas(canvas);
+        if (!isStart()) {
+            return;
+        }
+        if (!isFinish()) {
+            drawOneFrame(canvas);
+        } else {
+            onFrameAnimationEnd();
+        }
+    }
+
+    /**
+     * draw a single frame which is a bitmap
+     *
+     * @param canvas
+     */
+    private void drawOneFrame(Canvas canvas) {
+        Log.v("ttaylor", "ProgressRingSurfaceView.onFrameDraw()" + "  bitmapIndex=" + bitmapIndex + " measureWidth=" + getMeasuredWidth());
+        frameBitmap = decodeOriginBitmap(getResources(), bitmaps.get(bitmapIndex), options);
+        options.inBitmap = frameBitmap;
+        canvas.drawBitmap(frameBitmap, srcRect, dstRect, paint);
+        bitmapIndex++;
+    }
+
+    /**
+     * invoked when frame animation is done
+     */
+    private void onFrameAnimationEnd() {
+        reset();
+    }
+
+    /**
+     * reset the index of bitmap, preparing for the next frame animation
+     */
+    private void reset() {
+        bitmapIndex = INVALID_BITMAP_INDEX;
+    }
+
+    /**
+     * whether frame animation is finished
+     *
+     * @return true: animation is finished, false: animation is doing
+     */
+    private boolean isFinish() {
+        return bitmapIndex >= bitmaps.size();
+    }
+
+    /**
+     * whether frame animation is started
+     *
+     * @return true: animation is started, false: animation is not started
+     */
+    private boolean isStart() {
+        return bitmapIndex != INVALID_BITMAP_INDEX;
+    }
+
+    /**
+     * start frame animation which means draw list of bitmaps from 0 index
+     */
+    public void start() {
+        bitmapIndex = 0;
+    }
+
+
+    /**
+     * clear out the drawing on canvas,preparing for the next frame
+     * * @param canvas
+     */
+    private void clearCanvas(Canvas canvas) {
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        canvas.drawPaint(paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC));
+    }
+
+
+    private Bitmap decodeOriginBitmap(Resources res, int resId, BitmapFactory.Options options) {
+        options.inScaled = false;
+        Bitmap bitmap = BitmapFactory.decodeResource(res, resId, options);
+        return bitmap;
+    }
+}

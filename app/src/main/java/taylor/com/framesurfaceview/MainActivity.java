1fdf8e515b8310161df0e5a253464f27f3b45c8d
taylor <liang.tang@cootek.cn>
2019-05-08 18:45:20 +0800

    [test] test faster-frame-animation with 1mb frame

--- app/src/main/java/taylor/com/framesurfaceview/MainActivity.java
+++ app/src/main/java/taylor/com/framesurfaceview/MainActivity.java
@@ -16,32 +16,32 @@ import taylor.lib.framesurfaceview.NumberUtil;
 import taylor.lib.framesurfaceview.FrameSurfaceView;
 import taylor.lib.framesurfaceview.MethodUtil;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     private FrameSurfaceView frameSurfaceView;
 
-    private List<Integer> bitmaps = Arrays.asList(R.drawable.watch_reward_1,
-            R.drawable.watch_reward_2,
-            R.drawable.watch_reward_3,
-            R.drawable.watch_reward_4,
-            R.drawable.watch_reward_5,
-            R.drawable.watch_reward_6,
-            R.drawable.watch_reward_7,
-            R.drawable.watch_reward_8,
-            R.drawable.watch_reward_9,
-            R.drawable.watch_reward_10,
-            R.drawable.watch_reward_11,
-            R.drawable.watch_reward_12,
-            R.drawable.watch_reward_13,
-            R.drawable.watch_reward_14,
-            R.drawable.watch_reward_15,
-            R.drawable.watch_reward_16,
-            R.drawable.watch_reward_17,
-            R.drawable.watch_reward_18,
-            R.drawable.watch_reward_19,
-            R.drawable.watch_reward_20,
-            R.drawable.watch_reward_21,
-            R.drawable.watch_reward_22);
+    private List<Integer> bitmaps = Arrays.asList(
+            R.raw.frame0,
+            R.raw.frame1,
+            R.raw.frame2,
+            R.raw.frame3,
+            R.raw.frame4,
+            R.raw.frame5,
+            R.raw.frame6,
+            R.raw.frame7,
+            R.raw.frame8,
+            R.raw.frame9,
+            R.raw.frame10,
+            R.raw.frame11,
+            R.raw.frame12,
+            R.raw.frame13,
+            R.raw.frame14,
+            R.raw.frame15,
+            R.raw.frame16,
+            R.raw.frame17,
+            R.raw.frame18,
+            R.raw.frame19
+    );
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
